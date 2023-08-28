1aeb7b199d45e148002ff4066aac50c1ac9ca936
Raihan <raihanhn20@gmail.com>
2023-08-28 12:45:01 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Parallax } from "react-parallax";
+import img1 from "./images/waterfall.jpg";
+import img2 from "./images/pexels-suzy-hazelwood-12517600.jpg";
+import img3 from "./images/pexels-yuanpang-wa-15596359.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax strength={300} className="" bgImage={img1}>
+        <div className="content">
+          <div className="text-content">Normal parallax</div>
+        </div>
+      </Parallax>
+
+      <Parallax
+        strength={300}
+        blur={{ min: -5, max: 15 }}
+        className=""
+        bgImage={img2}
+      >
+        <div className="content">
+          <div className="text-content">Blur parallax</div>
+        </div>
+      </Parallax>
+
+      <Parallax strength={-600} bgImage={img3}>
+        <div className="content">
+          <div className="text-content">reverse parallax</div>
+        </div>
+      </Parallax>
+
+      <div className="content"></div>
     </div>
   );
 }
